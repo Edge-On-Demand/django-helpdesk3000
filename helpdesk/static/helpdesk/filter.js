9a6b624f36be558cf24008c3aef938780b5522fe
chris <chris@coronis>
2013-10-02 12:09:34 -0400

    Refactored media to be centralized in the base template.

--- helpdesk/static/helpdesk/filter.js
+++ helpdesk/static/helpdesk/filter.js
@@ -1,11 +1,12 @@
 (function($){
-    $(document).ready(function() {
-        $("#filterBuilderButton").click(function() {
+    //$(document).ready(function() {
+    $(document).bind('afterready', function() {
+        $("#searchtabs #filterBuilderButton").click(function() {
             var boxName = "#filterBox" + $("#filterBuilderSelect").val();
             $(boxName).slideDown();
             return false;
         });
-        $(".filterBuilderRemove").click(function() {
+        $("#searchtabs .filterBuilderRemove").click(function() {
             var boxName = "#" + $(this).parents(".filterBox").attr('id');
             $(boxName).slideUp();
             $(boxName).children("input:text").each(function() {
